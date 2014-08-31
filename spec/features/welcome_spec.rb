3dcd243b324728b6a0e41e39046c93ab631307ee
dB <dblock@dblock.org>
2014-08-31 12:21:44 -0400

    Always display links to blog, docs and help.

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,10 +1,24 @@
 require 'spec_helper'
 
 describe 'Welcome' do
-  it 'renders a welcome page' do
-    visit '/'
-    expect(page.body).to include 'The Art World in Your App'
-    expect(page.body).to include 'Sign In'
+  context 'page' do
+    before do
+      visit '/'
+    end
+    it 'renders a welcome message' do
+      expect(page.body).to include 'The Art World in Your App'
+    end
+    {
+      'Blog' => 'http://artsy.github.io',
+      'Docs' => '/docs',
+      'Help' => '/help',
+      'Sign In' => '/sign_in',
+      'Terms' => '/terms'
+    }.each_pair do |text, link|
+      it "links to #{text} from the nav bar" do
+        expect(page).to have_css ".navbar a[href='#{link}']", text: text
+      end
+    end
   end
   context 'sign in' do
     before do
