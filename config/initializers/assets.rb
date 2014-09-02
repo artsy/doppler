63b4f40102dc8b7139ec5885e7461e80a551f824
dB <dblock@dblock.org>
2014-09-01 22:05:33 -0400

    Added fonts to precompiled assets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,2 @@
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile = ['*.js', '*.css', '*.png']
+Rails.application.config.assets.precompile = ['*.js', '*.css', '*.png', '*.eot', '*.woff', '*.ttf']
