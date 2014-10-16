67165965d90705443a41efedf0204ea30aeecc48
dblock <dblock@dblock.org>
2014-10-15 20:48:16 -0400

    Use and describe with Hyperclient HEAD.

--- app/helpers/artsy_api.rb
+++ app/helpers/artsy_api.rb
@@ -13,9 +13,9 @@ module ArtsyAPI
 
   def self.client(options = {})
     Hyperclient.new(artsy_api_root).tap do |api|
-      api.headers.update('Accept' => 'application/vnd.artsy-v2+json')
-      api.headers.update('X-Access-Token' => options[:access_token]) if options.key?(:access_token)
-      api.headers.update('X-Xapp-Token' => options[:xapp_token]) if options.key?(:xapp_token)
+      api.headers['Accept'] = 'application/vnd.artsy-v2+json'
+      api.headers['X-Access-Token'] = options[:access_token] if options.key?(:access_token)
+      api.headers['X-Xapp-Token'] = options[:xapp_token] if options.key?(:xapp_token)
     end
   end
 
@@ -27,9 +27,7 @@ module ArtsyAPI
 
   def self.xapp_token
     Rails.cache.fetch "xapp-token/#{ENV['ARTSY_API_CLIENT_ID']}", expires_in: 1.hour do
-      client.connection.post("#{artsy_api_root}/tokens/xapp_token",
-                             client_id: ENV['ARTSY_API_CLIENT_ID'],
-                             client_secret: ENV['ARTSY_API_CLIENT_SECRET']).body['token']
+      api.tokens.xapp_token._post(client_id: ENV['ARTSY_API_CLIENT_ID'], client_secret: ENV['ARTSY_API_CLIENT_SECRET']).token
     end
   end
 end
