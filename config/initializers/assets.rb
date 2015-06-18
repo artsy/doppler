c47bd317ce5e23cbdf492cc48d2c2fcffec9d34d
Matthew Zikherman <matt@bestvendor.com>
2015-06-18 12:37:02 -0400

    Upgrade to Rails 4.2.2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile = ['*.js', '*.css', '*.png', '*.eot', '*.woff', '*.ttf']
+Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/swagger-ui"
