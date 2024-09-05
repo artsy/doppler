4f9f4903446f7a63e6bcd0e33af8d219b0bac686
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2024-09-05 09:53:27 -0400

    chore: Upgrades ddtrace gem and config (#299)

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,6 +1,9 @@
 Datadog.configure do |c|
-  c.tracer enabled: ENV["DATADOG_ENABLED"].present?, hostname: ENV["TRACE_AGENT_HOSTNAME"] || "datadog", distributed_tracing: true, debug: false
-  c.use :rails, service_name: "doppler", controller_service: "doppler.controller", cache_service: "doppler.cache", database_service: "doppler.postgres"
-  c.use :redis, service_name: "doppler.redis"
-  c.use :http, service_name: "doppler.http", distributed_tracing: true
+  c.tracing.enabled = ENV["DATADOG_ENABLED"].present?
+  c.agent.host = ENV["TRACE_AGENT_HOSTNAME"] || "datadog"
+  c.diagnostics.debug = false
+
+  c.tracing.instrument :rails, service_name: "doppler", controller_service: "doppler.controller", cache_service: "doppler.cache", database_service: "doppler.postgres"
+  c.tracing.instrument :redis, service_name: "doppler.redis"
+  c.tracing.instrument :http, service_name: "doppler.http", distributed_tracing: true
 end
