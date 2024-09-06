42aeaeb8afaf12012b21acca963e8fa74a82f649
Jackie Potts <jacqueline.a.potts@gmail.com>
2024-09-06 14:55:58 -0400

    Pin to more recent version of geckodriver

--- script/ci.sh
+++ script/ci.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set +x
-wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz -O /tmp/geckodriver.tar.gz && \
+wget https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux64.tar.gz -O /tmp/geckodriver.tar.gz && \
   tar xvf /tmp/geckodriver.tar.gz -C /opt && \
   ln -fs /opt/geckodriver /usr/bin/geckodriver
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,25 +8,4 @@ Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
-
-  Capybara.register_driver :headless_chrome do |app|
-    # Create the service with the path to chromedriver
-    service = Selenium::WebDriver::Service.chrome(
-      path: ENV.fetch("CHROMEDRIVER_PATH", "/usr/bin/chromedriver")
-    )
-
-    # Set up Chrome options for headless mode
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.args << "headless"
-    options.args << "disable-gpu"
-    options.args << "no-sandbox"
-    options.args << "disable-dev-shm-usage"
-
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options, service: service)
-  end
-
-  Capybara.configure do |config|
-    config.javascript_driver = :headless_chrome
-    config.default_max_wait_time = 10
-  end
 end
