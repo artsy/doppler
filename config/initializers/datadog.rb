4a4d657f30ede43dfa02572c8fa8563892c5b4f2
Jackie Potts <jacqueline.a.potts@gmail.com>
2024-09-09 17:14:26 -0400

    Roll back bootstrap changes

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,6 +1,6 @@
-Datadog.configure do |c|
-  c.tracer enabled: ENV["DATADOG_ENABLED"].present?, hostname: ENV["TRACE_AGENT_HOSTNAME"] || "datadog", distributed_tracing: true, debug: false
-  c.use :rails, service_name: "doppler", controller_service: "doppler.controller", cache_service: "doppler.cache", database_service: "doppler.postgres"
-  c.use :redis, service_name: "doppler.redis"
-  c.use :http, service_name: "doppler.http", distributed_tracing: true
-end
+# Datadog.configure do |c|
+#   c.tracer enabled: ENV["DATADOG_ENABLED"].present?, hostname: ENV["TRACE_AGENT_HOSTNAME"] || "datadog", distributed_tracing: true, debug: false
+#   c.use :rails, service_name: "doppler", controller_service: "doppler.controller", cache_service: "doppler.cache", database_service: "doppler.postgres"
+#   c.use :redis, service_name: "doppler.redis"
+#   c.use :http, service_name: "doppler.http", distributed_tracing: true
+# end
