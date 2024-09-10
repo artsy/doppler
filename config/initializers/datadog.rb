c4acf192fec80880c2c62bf5ac7b4810da6c777f
Jackie Potts <jacqueline.a.potts@gmail.com>
2024-09-10 11:56:33 -0400

    Updates rails dd config to reenable controller reporting

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -3,7 +3,17 @@ Datadog.configure do |c|
   c.agent.host = ENV["TRACE_AGENT_HOSTNAME"] || "datadog"
   c.diagnostics.debug = false
 
-  c.tracing.instrument :rails, service_name: "doppler", controller_service: "doppler.controller", cache_service: "doppler.cache", database_service: "doppler.postgres"
+  c.tracing.instrument :rails do |rails|
+    rails.service_name = "doppler"
+    rails.controller_service = "doppler.controller"
+    rails.cache_service = "doppler.cache"
+    rails.database_service = "doppler.postgres"
+  end
+
   c.tracing.instrument :redis, service_name: "doppler.redis"
-  c.tracing.instrument :http, service_name: "doppler.http", distributed_tracing: true
+
+  c.tracing.instrument :http do |http|
+    http.service_name = "doppler.http"
+    http.distributed_tracing = true
+  end
 end
