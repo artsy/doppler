0424ec090877aede4b6f0c0135ab0aae8bb6487e
Jackie Potts <jacqueline.a.potts@gmail.com>
2024-10-09 11:03:25 -0400

    Remove custom JS for less fun

--- app/assets/javascripts/webhook_deliveries.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const rows = document.querySelectorAll('.clickable-row');
-  rows.forEach(row => {
-    row.addEventListener('click', () => {
-      window.location.href = row.getAttribute('data-url');
-    })
-  })
-})
