952cd3135f2674595431f096135e147cf2adeddc
Jackie Potts <jacqueline.a.potts@gmail.com>
2024-10-30 16:40:08 -0400

    Start allowing clients to attempt webhook redelivery

--- app/services/client_application_service.rb
+++ app/services/client_application_service.rb
@@ -8,4 +8,9 @@ class ClientApplicationService
     url = "#{Gravity::GRAVITY_V1_API_URL}/webhook_delivery/#{params[:id]}"
     Gravity.get(url: url, additional_headers: {"X-Access-Token" => access_token}, params: params)
   end
+
+  def self.redeliver_webhook_delivery(access_token, params = {})
+    url = "#{Gravity::GRAVITY_V1_API_URL}/webhook_delivery/#{params[:id]}/redeliver"
+    Gravity.post(url: url, additional_headers: {"X-Access-Token" => access_token}, params: params)
+  end
 end
